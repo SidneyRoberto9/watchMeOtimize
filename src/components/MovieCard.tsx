e68df48d1590714e808416df5c9058942bfca122
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-03-17 15:58:44 -0300

    🎉 initial: initial commit

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,33 @@
+import '../styles/movie-card.scss';
+
+import { Clock, Star } from 'react-feather';
+
+interface MovieCardProps {
+  title: string;
+  poster: string;
+  rating: string;
+  runtime: string;
+}
+
+export function MovieCard(props: MovieCardProps) {
+  return (
+    <div className="movie-card">
+      <img src={props.poster} alt={props.title} />
+
+      <div>
+        <div className="movie-info">
+          <span>{props.title}</span>
+          <div className="meta">
+            <div>
+              <Star /> {props.rating}
+            </div>
+
+            <div>
+              <Clock /> {props.runtime}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,39 @@
+import { Button } from './Button';
+
+interface GenreData {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: Array<GenreData>;
+  selectedGenreId: number;
+  buttonClickCallback: (args: any) => void;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  buttonClickCallback,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => buttonClickCallback(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
